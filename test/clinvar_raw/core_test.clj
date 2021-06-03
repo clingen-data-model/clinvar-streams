fdfadef7a845c407b9b68ce9f480354bdd632bcc
Kyle Ferriter <kferrite@broadinstitute.org>
2021-06-03 13:20:42 -0400

    Fix clinvar_raw/core_test which was out of sync with changes to clinvar_raw/core

--- test/clinvar_raw/core_test.clj
+++ test/clinvar_raw/core_test.clj
@@ -4,9 +4,10 @@
             [cheshire.core :as json]
             [taoensso.timbre :as log]
             [clojure.java.io :as io]
+            [clojure.data :refer [diff]]
             [clojure.core.async :as async :refer [>!! <!! poll!]]
             [clinvar-raw.core :as core]
-            [clinvar-raw.util :as util]))
+            [clinvar-streams.util :as util]))
 
 ;(deftest bucket-test
 ;  (testing "Ensure bucket is set to correct value"
@@ -20,74 +21,70 @@
 (def release-date "2020-04-01")
 
 (def drop-file-records
-  {
-   :gene
-                                   {
-                                    :processed-clinvar-drop
-                                    [{
-                                      :key (str "gene_107984974_" release-date)
-                                      :data
-                                           {
-                                            :time    release-date
-                                            :type    "created"
-                                            :content (assoc
-                                                       (json/parse-string (slurp "test/resources/drop_files/created/good/gene.json") true)
-                                                       :entity_type "gene")
-                                            }
-                                      }]
-                                    }
-   :variation                      ""
-   :gene_association               ""
-   :variation_archive              ""
-   :trait                          ""
-   :trait_set                      ""
-   :rcv_accession                  ""
-   :submitter                      ""
-   :submission                     ""
-   :clinical_assertion             ""
-   :clinical_assertion_variation   ""
-   :clinical_assertion_trait       ""
-   :clinical_assertion_trait_set   ""
+  {:gene
+   {:processed-clinvar-drop
+    [{:key (str "gene_107984974_" release-date)
+      :value
+      {:release_date release-date
+       :event_type "created"
+       :content (assoc
+                  (json/parse-string (slurp "test/clinvar_raw/resources/drop_files/created/good/gene.json") true)
+                  :entity_type "gene"
+                  :clingen_version 0)}
+      }]
+    }
+   :variation ""
+   :gene_association ""
+   :variation_archive ""
+   :trait ""
+   :trait_set ""
+   :rcv_accession ""
+   :submitter ""
+   :submission ""
+   :clinical_assertion ""
+   :clinical_assertion_variation ""
+   :clinical_assertion_trait ""
+   :clinical_assertion_trait_set ""
    :clinical_assertion_observation ""
-   :trait_mapping                  ""
+   :trait_mapping ""
    })
 
 (defn get-drop-file-records [entity-type]
-  (let [contents (slurp (str "test/resources/drop_files/created/good/" (str entity-type ".json")))
+  (let [contents (slurp (str "test/clinvar_raw/resources/drop_files/created/good/" (str entity-type ".json")))
         lines (s/split-lines contents)]
     (filter #(< 0 (.length %)) lines)
     ))
 
 (def clinical-assertion-event {:key "clinical_assertion_SCV000924344_2020-01-01T12:00:00Z",
                                :data
-                                    {:time "2020-01-01T12:00:00Z",
-                                     :type "create",
-                                     :content
-                                           {:variation_id                       "634922",
-                                            :variation_archive_id               "VCV000634922",
-                                            :submitter_id                       "3",
-                                            :date_last_updated                  "2019-06-24",
-                                            :interpretation_comments            [],
-                                            :interpretation_description         "risk factor",
-                                            :trait_set_id                       "47782",
-                                            :internal_id                        "1807657",
-                                            :type                               "clinical_assertion",
-                                            :submission_id                      "3.2019-06-18",
-                                            :local_key
-                                                                                "114350.0001_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
-                                            :clinical_assertion_observation_ids ["SCV000924344.0"],
-                                            :title
-                                                                                "NUP214, ASP154GLY_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
-                                            :assertion_type                     "variation to disease",
-                                            :rcv_accession_id                   "RCV000785779",
-                                            :clinical_assertion_trait_set_id    "SCV000924344",
-                                            :id                                 "SCV000924344",
-                                            :submission_names                   [],
-                                            :record_status                      "current",
-                                            :date_created                       "2019-06-20",
-                                            :review_status                      "no assertion criteria provided",
-                                            :interpretation_date_last_evaluated "2019-06-18",
-                                            :version                            "1"}}})
+                               {:release_date "2020-01-01T12:00:00Z",
+                                :type "create",
+                                :content
+                                {:variation_id "634922",
+                                 :variation_archive_id "VCV000634922",
+                                 :submitter_id "3",
+                                 :date_last_updated "2019-06-24",
+                                 :interpretation_comments [],
+                                 :interpretation_description "risk factor",
+                                 :trait_set_id "47782",
+                                 :internal_id "1807657",
+                                 :type "clinical_assertion",
+                                 :submission_id "3.2019-06-18",
+                                 :local_key
+                                 "114350.0001_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
+                                 :clinical_assertion_observation_ids ["SCV000924344.0"],
+                                 :title
+                                 "NUP214, ASP154GLY_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
+                                 :assertion_type "variation to disease",
+                                 :rcv_accession_id "RCV000785779",
+                                 :clinical_assertion_trait_set_id "SCV000924344",
+                                 :id "SCV000924344",
+                                 :submission_names [],
+                                 :record_status "current",
+                                 :date_created "2019-06-20",
+                                 :review_status "no assertion criteria provided",
+                                 :interpretation_date_last_evaluated "2019-06-18",
+                                 :version "1"}}})
 
 ; TODO Update to use all entity-types when spec in clinvar-raw.core is complete
 (deftest test-line-to-event
@@ -97,9 +94,37 @@
           datetime "2020-01-01T12:00:00Z"
           event-type "create"
           ;expected-value {:key "SCV000924344_2020-01-01T12:00:00Z", :value "{\"time\":\"2020-01-01T12:00:00Z\",\"type\":\"create\",\"content\":{\"variation_id\":\"634922\",\"variation_archive_id\":\"VCV000634922\",\"submitter_id\":\"3\",\"date_last_updated\":\"2019-06-24\",\"interpretation_comments\":[],\"interpretation_description\":\"risk factor\",\"trait_set_id\":\"47782\",\"internal_id\":\"1807657\",\"type\":\"clinical_assertion\",\"submission_id\":\"3.2019-06-18\",\"local_key\":\"114350.0001_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9\",\"clinical_assertion_observation_ids\":[\"SCV000924344.0\"],\"title\":\"NUP214, ASP154GLY_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9\",\"assertion_type\":\"variation to disease\",\"rcv_accession_id\":\"RCV000785779\",\"clinical_assertion_trait_set_id\":\"SCV000924344\",\"id\":\"SCV000924344\",\"submission_names\":[],\"record_status\":\"current\",\"date_created\":\"2019-06-20\",\"review_status\":\"no assertion criteria provided\",\"interpretation_date_last_evaluated\":\"2019-06-18\",\"version\":\"1\"}}"}]
-          expected-value {:key "clinical_assertion_SCV000924344_2020-01-01T12:00:00Z", :data {:time "2020-01-01T12:00:00Z", :type "create", :content {:variation_id "634922", :variation_archive_id "VCV000634922", :submitter_id "3", :date_last_updated "2019-06-24", :interpretation_comments [], :interpretation_description "risk factor", :trait_set_id "47782", :internal_id "1807657", :entity_type "clinical_assertion", :submission_id "3.2019-06-18", :local_key "114350.0001_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9", :clinical_assertion_observation_ids ["SCV000924344.0"], :title "NUP214, ASP154GLY_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9", :assertion_type "variation to disease", :rcv_accession_id "RCV000785779", :clinical_assertion_trait_set_id "SCV000924344", :id "SCV000924344", :submission_names [], :record_status "current", :date_created "2019-06-20", :review_status "no assertion criteria provided", :interpretation_date_last_evaluated "2019-06-18", :version "1"}}}]
+          expected-value {:key "clinical_assertion_SCV000924344_2020-01-01T12:00:00Z",
+                          :value {:release_date "2020-01-01T12:00:00Z",
+                                  :event_type "create",
+                                  :content {:clingen_version 0
+                                            :variation_id "634922",
+                                            :variation_archive_id "VCV000634922",
+                                            :submitter_id "3",
+                                            :date_last_updated "2019-06-24",
+                                            :interpretation_comments [],
+                                            :interpretation_description "risk factor",
+                                            :trait_set_id "47782",
+                                            :internal_id "1807657",
+                                            :entity_type "clinical_assertion",
+                                            :submission_id "3.2019-06-18",
+                                            :local_key "114350.0001_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
+                                            :clinical_assertion_observation_ids ["SCV000924344.0"],
+                                            :title "NUP214, ASP154GLY_ENCEPHALOPATHY, ACUTE, INFECTION-INDUCED, SUSCEPTIBILITY TO, 9",
+                                            :assertion_type "variation to disease",
+                                            :rcv_accession_id "RCV000785779",
+                                            :clinical_assertion_trait_set_id "SCV000924344",
+                                            :id "SCV000924344",
+                                            :submission_names [],
+                                            :record_status "current",
+                                            :date_created "2019-06-20",
+                                            :review_status "no assertion criteria provided",
+                                            :interpretation_date_last_evaluated "2019-06-18",
+                                            :version "1"}}}]
       (let [actual-value (core/line-map-to-event (json/parse-string line true) entity-type datetime event-type)]
-        (is (= expected-value actual-value)))
+        (is (= expected-value actual-value)
+            (str "Expected did not match actual: "
+                 (into [] (diff expected-value actual-value)))))
       )))
 
 (deftest test-filter-files
@@ -135,12 +160,19 @@
       ; For each file, open a reader and run process-clinvar-drop-file on it
       ; check return seq value literals
       (doseq [entity-type entity-types]
-        (with-open [r (io/reader (str "test/resources/drop_files/created/good/" entity-type ".json"))]
+        (with-open [r (io/reader (str "test/clinvar_raw/resources/drop_files/created/good/" entity-type ".json"))]
           (let [expected-value (:processed-clinvar-drop ((keyword entity-type) drop-file-records))]
-            (core/process-clinvar-drop-file {:reader r :entity-type entity-type :datetime release-date :event-type "created"})
+            (core/process-clinvar-drop-file {:reader r
+                                             :entity-type entity-type
+                                             :release_date release-date
+                                             :event-type "created"
+                                             :filter-field nil})
             (Thread/sleep 100)                              ; TODO put spin
             (let [actual-value (<!! (async/into [] (async/take (count expected-value) core/producer-channel)))]
               (println actual-value)
-              (is (= expected-value actual-value)))
+              (is (= expected-value actual-value)
+                  (str "Expected did not match actual: "
+                       (into [] (diff expected-value actual-value)))
+                  ))
             ))))
     ))
