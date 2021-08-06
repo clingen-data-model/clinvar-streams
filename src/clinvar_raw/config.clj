19b8fbe28ee7c16e209ebc999041f3471e0b869e
Kyle Ferriter <kferrite@broadinstitute.org>
2021-08-06 14:20:42 -0400

    Load raw input/output topic names from env

--- src/clinvar_raw/config.clj
+++ src/clinvar_raw/config.clj
@@ -1,5 +1,6 @@
 (ns clinvar-raw.config
-  (:require [taoensso.timbre :as timbre])
+  (:require [taoensso.timbre :as timbre]
+            [clinvar-streams.util :as util])
   (:import java.lang.System)
   (:gen-class))
 
@@ -7,12 +8,13 @@
 
 (defn app-config
   []
-  {:kafka-host                  "pkc-4yyd6.us-east1.gcp.confluent.cloud:9092"
-   :kafka-user                  (System/getenv "KAFKA_USER")
-   :kafka-password              (System/getenv "KAFKA_PASSWORD")
-   :kafka-group                 (or (System/getenv "KAFKA_GROUP") "clinvar-raw")
-   :kafka-consumer-topic        "broad-dsp-clinvar"
-   :kafka-producer-topic        "clinvar-raw"
+  {:kafka-host (or (System/getenv "KAFKA_BROKER")
+                   "pkc-4yyd6.us-east1.gcp.confluent.cloud:9092")
+   :kafka-user (util/get-env-required "KAFKA_USER")
+   :kafka-password (util/get-env-required "KAFKA_PASSWORD")
+   :kafka-group (util/get-env-required "KAFKA_GROUP")
+   :kafka-consumer-topic (util/get-env-required "DX_CV_RAW_INPUT_TOPIC")
+   :kafka-producer-topic (util/get-env-required "DX_CV_RAW_OUTPUT_TOPIC")
    :kafka-reset-consumer-offset (Boolean/valueOf (System/getenv "KAFKA_RESET_CONSUMER_OFFSET"))
    })
 
@@ -20,18 +22,18 @@
   "Expects :kafka-user, :kafka-password, :kafka-host, :kafka-group in opts."
   [opts]
   {"ssl.endpoint.identification.algorithm" "https"
-   "compression.type"                      "gzip"
-   "sasl.mechanism"                        "PLAIN"
-   "request.timeout.ms"                    "20000"
-   "bootstrap.servers"                     (:kafka-host opts)
-   "retry.backoff.ms"                      "500"
-   "security.protocol"                     "SASL_SSL"
-   "key.serializer"                        "org.apache.kafka.common.serialization.StringSerializer"
-   "value.serializer"                      "org.apache.kafka.common.serialization.StringSerializer"
-   "key.deserializer"                      "org.apache.kafka.common.serialization.StringDeserializer"
-   "value.deserializer"                    "org.apache.kafka.common.serialization.StringDeserializer"
-   "group.id"                              (:kafka-group opts)
-   "sasl.jaas.config"                      (str "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""
-                                                (:kafka-user opts) "\" password=\"" (:kafka-password opts) "\";")})
+   "compression.type" "gzip"
+   "sasl.mechanism" "PLAIN"
+   "request.timeout.ms" "20000"
+   "bootstrap.servers" (:kafka-host opts)
+   "retry.backoff.ms" "500"
+   "security.protocol" "SASL_SSL"
+   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "group.id" (:kafka-group opts)
+   "sasl.jaas.config" (str "org.apache.kafka.common.security.plain.PlainLoginModule required username=\""
+                           (:kafka-user opts) "\" password=\"" (:kafka-password opts) "\";")})
 
 (def date-format "YYYY-MM-DD'T'HH:mm:ss:Z")
