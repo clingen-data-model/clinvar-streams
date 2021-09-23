7b4c226c0219091649c806e60b0889a96495a3aa
Kyle Ferriter <kferrite@broadinstitute.org>
2021-09-23 10:51:13 -0400

    Remove dead code

--- src/clinvar_combiner/combiners/core.clj
+++ src/clinvar_combiner/combiners/core.clj
@@ -11,52 +11,6 @@
   (:import (java.sql PreparedStatement ResultSet)
            (java.util Iterator)))
 
-;(defn get-simple [table-name]
-;  (let [ct (:ct (first (jdbc/query @db-client/db
-;                                   [(format "select count(*) as ct from %s where dirty = 1" table-name)])))
-;        s (str (format "select * from %s where dirty = 1 " table-name)
-;               " order by id, release_date asc limit %s offset %s ")
-;        batch-size 100
-;        batch-count (Math/ceil (/ ct batch-size))]
-;    (let [last-id-pk (atom nil)
-;          last-release-date-pk (atom nil)]
-;      (for [batch-num (range batch-count)]
-;        (let [offset (* batch-size batch-num)
-;              limit batch-size
-;              s-batch (format s limit offset)]
-;          (jdbc/query @db-client/db [s-batch]))))))
-
-
-;(defn get-simple
-;  ;"For tables indexed by id and release_date"
-;  ([table-name] (get-simple table-name {}))
-;  ([table-name last-pks]
-;   (let [s (str (format "select * from %s where dirty = 1 " table-name))
-;         pk-where-templ " and (id, release_date) > (?, ?) "
-;         order-clause " order by id asc, release_date asc "
-;         batch-size 1000
-;         limit-clause (format " limit %s " batch-size)
-;         ]
-;     (let [last-id-pk (:id last-pks)
-;           last-release-date-pk (:release_date last-pks)]
-;       (let [s-batch (str (format s table-name)
-;                          (if (not-empty last-pks)
-;                            (str pk-where-templ))
-;                          order-clause
-;                          limit-clause)
-;             params (if (not-empty last-pks)
-;                      [s-batch last-id-pk last-release-date-pk]
-;                      [s-batch])]
-;         (log/info "Running query: " s-batch)
-;         (log/info "params: " (rest params))
-;         (let [rs (jdbc/query @db-client/db params)]
-;           (if (not= 0 (count rs))
-;             (lazy-cat
-;               rs
-;               (get-simple table-name (select-keys (last rs)
-;                                                   [:id :release_date])))
-;             )
-;           ))))))
 
 (defn resultset-iterator
   ([^ResultSet rs] (resultset-iterator rs {}))
@@ -66,12 +20,8 @@
          indices (mapv inc (range (.getColumnCount meta)))
          col-names (mapv #(.getColumnName meta %) indices)]
      (letfn [(rs-datify [^ResultSet rs]
-               (let [;meta (.getMetaData rs)
-                     ;col-names (map #(.getColumnName meta %) (map inc (range (.getColumnCount meta))))
-                     col-values (map #(.getObject rs %) indices)]
+               (let [col-values (map #(.getObject rs %) indices)]
                  (let [col-name-kw (map keyword col-names)]
-                   ;(log/info {:col-names col-name-kw})
-                   ;(log/info {:col-values col-values})
                    (into {} (map vector col-name-kw col-values)))))]
        (reify Iterator
          (next [this]
@@ -159,20 +109,16 @@
                   rs-iterator (execute-to-rs-iterator! sql [])]
               (iterator-seq rs-iterator)))
           (get-variation []
-            (let [;dirty-variations (get-dirty-root-variations)
-                  ;dirty-variations-with-gene-associations (map add-variation-gene-association dirty-variations)
-                  ])
-
             (->> (get-dirty-root-variations)
                  (map add-variation-gene-association)
                  (map #(cons % (get-variation-descendants %)))
-                 (map (fn[%] (log/info %) %))
+                 (map (fn [%] (log/info %) %))
                  (map variation-list-to-compound)
                  (map (fn [%] (if (< 1 (count %))
                                 (throw (ex-info "Multiple records returned from variation-list-to-compound"
                                                 {:value %}))
                                 (first %))))
-                 (map (fn[%] (log/info (into [] %)) %))
+                 (map (fn [%] (log/info (into [] %)) %))
                  ))]
     (get-variation)))
 
@@ -182,44 +128,19 @@
   [release-sentinel]
   (log/info "Getting all dirty non-scv records from release: " (str release-sentinel))
   (let [release-date (:release_date release-sentinel)]
-    (letfn [
-            ;(->> dirty-variations
-            ;     ; Set top level release date to max and remove from gene associations
-            ;     (map (fn [variation]
-            ;            (let [max-release-date (apply obj-max
-            ;                                          (concat [(:release_date variation)]
-            ;                                                  (map :release_date (:gene_associations variation))))]
-            ;              (-> variation
-            ;                  (assoc :release_date max-release-date)
-            ;                  ; If a gene association was deleted and the variation is not deleted
-            ;                  ; mark the variation as "update".
-            ;                  ; If the variation was deleted, still pass it on with the new state
-            ;                  ; of the gene associations, but keeping the variation marked deleted.
-            ;                  ((fn [variation]
-            ;                     (let [was-gene-association-deleted
-            ;                           (some #(= "delete" %) (map :event_type (:gene_associations variation)))]
-            ;                       (if (and was-gene-association-deleted
-            ;                                (not= "delete" (:event_type variation)))
-            ;                         (assoc variation :event_type "update")
-            ;                         variation))))
-            ;                  (assoc :gene_associations (map #(dissoc % :release_date :event_type :dirty)
-            ;                                                 (:gene_associations variation)))))))
-            ;     (variation-list-to-compound)
-            ;     )
-            ]
-      (map
-        ; If record was event_type=delete set deleted=true. Remove event_type.
-        (fn [rec] (dissoc (if (= "delete" (:event_type rec))
-                            (assoc rec :deleted true)
-                            rec)
-                          :event_type))
-        (lazy-cat (map #(assoc % :entity_type "submitter") (get-simple "submitter"))
-                  (map #(assoc % :entity_type "submission") (get-simple "submission"))
-                  (map #(assoc % :entity_type "trait") (get-simple "trait"))
-                  (map #(assoc % :entity_type "trait_set") (get-simple "trait_set"))
-                  (map #(assoc % :entity_type "gene") (get-simple "gene"))
-                  (map #(assoc % :entity_type "variation") (get-dirty-variations)) ; Special case for gene_association
-                  (map #(assoc % :entity_type "variation_archive") (get-simple "variation_archive"))
-                  (map #(assoc % :entity_type "rcv_accession") (get-simple "rcv_accession")))))))
+    (map
+      ; If record was event_type=delete set deleted=true. Remove event_type.
+      (fn [rec] (dissoc (if (= "delete" (:event_type rec))
+                          (assoc rec :deleted true)
+                          rec)
+                        :event_type))
+      (lazy-cat (map #(assoc % :entity_type "submitter") (get-simple "submitter"))
+                (map #(assoc % :entity_type "submission") (get-simple "submission"))
+                (map #(assoc % :entity_type "trait") (get-simple "trait"))
+                (map #(assoc % :entity_type "trait_set") (get-simple "trait_set"))
+                (map #(assoc % :entity_type "gene") (get-simple "gene"))
+                (map #(assoc % :entity_type "variation") (get-dirty-variations)) ; Special case for gene_association
+                (map #(assoc % :entity_type "variation_archive") (get-simple "variation_archive"))
+                (map #(assoc % :entity_type "rcv_accession") (get-simple "rcv_accession"))))))
 
 
