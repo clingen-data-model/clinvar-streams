7e92d60e5e3cff232946d097d45b6a286d691cca
Kyle Ferriter <kferrite@broadinstitute.org>
2022-08-18 17:00:13 -0400

    Add 10 min backoff when poll returns no records. Increase max poll batch size.

--- src/clinvar_raw/stream_filter.clj
+++ src/clinvar_raw/stream_filter.clj
@@ -16,34 +16,46 @@
         entity-type (-> msg :content :entity_type)]
     (or (and (= "variation" entity-type)
              (contains? id-set (-> msg :content :id str)))
-        #_(and (= "clinical_assertion" entity-type)
-               (contains? id-set (-> msg :content :variation_id str)))
-        #_(and (= "variation_archive" entity-type)
-               (contains? id-set (-> msg :content :variation_id str))))))
+        (and (= "clinical_assertion" entity-type)
+             (contains? id-set (-> msg :content :variation_id str)))
+        (and (= "variation_archive" entity-type)
+             (contains? id-set (-> msg :content :variation_id str))))))
 
 (defn -main [& args]
   (let [app-config (cfg/app-config)
-        kafka-opts (cfg/kafka-config app-config)
+        kafka-opts (-> (cfg/kafka-config app-config)
+                       (assoc "max.poll.records" "1000"))
         consumer (jc/consumer kafka-opts)
         looping? (atom true)
         output-filename (str (:kafka-consumer-topic app-config) "-filtered.txt")
-        variation-ids (-> "vcepvars-220801.txt" io/reader line-seq set)]
+        variation-ids (-> "vcepvars-220801.txt" io/reader line-seq set)
+        max-empty-batches 10
+        empty-batch-count (atom 0)]
     (jc/subscribe consumer [{:topic-name (:kafka-consumer-topic app-config)}])
     (jc/poll consumer 0)
     (jc/seek-to-beginning-eager consumer)
     (with-open [writer (io/writer output-filename)]
       (while @looping?
-        (let [batch (jc/poll consumer (Duration/ofSeconds 5))]
-          (log/infof "Read %d records. First record: %s"
+        (let [batch (jc/poll consumer (Duration/ofSeconds 10))]
+          (log/infof "Read %s records. First record offset: %s, date: %s"
                      (count batch)
-                     (json/generate-string (-> batch first :value)))
-          (when (empty? batch)
-            (reset! looping? false))
-          (let [filtered (->> batch
-                              (map :value)
-                              (map #(json/parse-string % true))
-                              (filter #(predicate-variation-id-in % variation-ids)))]
-            (log/infof "Writing %d filtered records" (count filtered))
-            (dorun (map #(do (.write writer %)
-                             (.write writer "\n"))
-                        (map json/generate-string filtered)))))))))
+                     (-> batch first :offset)
+                     (json/generate-string
+                      (-> batch first :value (#(select-keys
+                                                (json/parse-string % true)
+                                                [:release_date])))))
+          (if (empty? batch)
+            (if (<= max-empty-batches @empty-batch-count)
+              (reset! looping? false)
+              (do (log/info "No records returned from poll, sleeping 1 minute")
+                  (swap! empty-batch-count inc)
+                  (Thread/sleep (* 1000 60))))
+            (do (reset! empty-batch-count 0)
+                (let [filtered (->> batch
+                                    (map :value)
+                                    (map #(json/parse-string % true))
+                                    (filter #(predicate-variation-id-in % variation-ids)))]
+                  (log/infof "Writing %d filtered records" (count filtered))
+                  (dorun (map #(do (.write writer %)
+                                   (.write writer "\n"))
+                              (map json/generate-string filtered)))))))))))
