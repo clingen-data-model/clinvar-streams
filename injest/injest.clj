4b5eddd588fc510bd9289ba189f2e6d4477951b4
Tom Lyons <tbl3rd@gmail.com>
2022-12-06 13:42:21 -0500

    Checkpoint new dependencies.

---
+++ injest/injest.clj
@@ -0,0 +1,41 @@
+(ns injest
+  "Mistakenly ingest stuff."
+  (:require [clojure.data.json  :as json]
+            [clojure.java.io    :as io]
+            [clojure.pprint     :refer [pprint]]
+            [clojure.spec.alpha :as s]
+            [clojure.string     :as str]))
+
+(s/def ::table
+  (s/and sequential?
+         (partial every? sequential?)
+         #(== 1 (count (set (map count %))))
+         #(== (count (set (first %))) (count (first %)))))
+
+(def ftp-site
+  "FTP site of the National Library of Medicine."
+  "https://ftp.ncbi.nlm.nih.gov")
+
+(def staging
+  "The bucket where Monster ingest stages the ClinVar files."
+  "gs://broad-dsp-monster-clingen-prod")
+
+(defn tabulate
+  "Return a vector of vectors from FILE as a Tab-Separated-Values table."
+  [file]
+  (letfn [(split [line] (str/split line #"\t"))]
+    (-> file io/reader line-seq
+        (->> (map split)))))
+
+(defn mapulate
+  "Return a sequence of labeled maps from the TSV TABLE."
+  [table]
+  {:pre [(s/valid? ::table table)]}
+  (let [[header & rows] table]
+    (map (partial zipmap header) rows)))
+
+(defn clinvar_releases_pre_20221027
+  "Return the TSV file as a sequence of maps."
+  []
+  (-> "./clinvar_releases_pre_20221027.tsv"
+      tabulate mapulate))
